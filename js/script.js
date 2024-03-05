2ecf00a3080c751b7d394415fe96d5ae4b682635
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-03-05 19:13:21 +0000

    change js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya Feng All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya Feng
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function  myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
